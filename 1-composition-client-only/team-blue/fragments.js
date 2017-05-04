db368ca88fa2b1958e9ee235d00bf07f983a87db
Michael Geers <michael.geers@neuland-bfi.de>
2017-05-04 17:11:14 +0200

    extracted order-stuff and recommendations into own custom elements

---
+++ 1-composition-client-only/team-blue/fragments.js
@@ -0,0 +1,78 @@
+/* eslint-disable no-use-before-define, no-console, class-methods-use-this */
+/* globals HTMLElement, window, CustomEvent */
+(function fragments() {
+  const prices = {
+    t_red: '66,00 €',
+    t_green: '54,00 €',
+    t_blue: '58,00 €',
+  };
+
+  const state = {
+    count: 0,
+  };
+
+  class BlueBasket extends HTMLElement {
+    connectedCallback() {
+      this.refresh = this.refresh.bind(this);
+      this.log('connected');
+      this.render();
+      window.addEventListener('blue:basket:changed', this.refresh);
+    }
+    refresh() {
+      this.log('event recieved "blue:basket:changed"');
+      this.render();
+    }
+    render() {
+      const classname = state.count === 0 ? 'empty' : 'filled';
+      this.innerHTML = `
+        <div class="${classname}">basket: ${state.count} item(s)</div>
+      `;
+    }
+    disconnectedCallback() {
+      window.removeEventListener('blue:basket:changed', this.refresh);
+      this.log('disconnected');
+    }
+    log(...args) {
+      console.log('🛒 blue-basket', ...args);
+    }
+  }
+  window.customElements.define('blue-basket', BlueBasket);
+
+
+  class BlueBuy extends HTMLElement {
+    connectedCallback() {
+      this.addToCart = this.addToCart.bind(this);
+      const sku = this.getAttribute('sku');
+      this.log('connected', sku);
+      this.render();
+      this.firstChild.addEventListener('click', this.addToCart);
+    }
+    addToCart() {
+      state.count += 1;
+      this.log('event sent "blue:basket:changed"');
+      this.dispatchEvent(new CustomEvent('blue:basket:changed', {
+        bubbles: true,
+      }));
+    }
+    render() {
+      const sku = this.getAttribute('sku');
+      const price = prices[sku];
+      this.innerHTML = `<button type="button">buy for ${price}</button>`;
+    }
+    attributeChangedCallback(attr, oldValue, newValue) {
+      this.log('attributeChanged', attr, oldValue, newValue);
+      if (attr === 'sku') {
+        this.render();
+      }
+    }
+    disconnectedCallback() {
+      this.firstChild.removeEventListener('click', this.addToCart);
+      const sku = this.getAttribute('sku');
+      this.log('disconnected', sku);
+    }
+    log(...args) {
+      console.log('🔘 blue-buy', ...args);
+    }
+  }
+  window.customElements.define('blue-buy', BlueBuy);
+}());
